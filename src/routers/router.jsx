1a208a51b0c27fa6b11cfb5c83a549dcd0d00468
Irfan Hassan <irfan.ridu1324@gmail.com>
2025-01-12 08:12:24 +0600

    book

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -8,7 +8,7 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
 import CheckoutPage from "../pages/books/CheckoutPage";
-import SingleBook from "../pages/books/singleBook";
+import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
 import AdminRoute from "./AdminRoute";
 import AdminLogin from "../components/AdminLogin";
